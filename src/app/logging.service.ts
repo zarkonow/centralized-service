1391119f7de2ba87fbc0dba08fc419c5a4549280
zarkonow <stojanov1987@gmail.com>
2025-05-02 01:58:08 +0200

    feat: Implement LoggingService and integrate logging in TasksService

---
+++ src/app/logging.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggingService {
+
+  log(message: string) {
+    const timeStamp = new Date().toLocaleTimeString()
+    console.log(`[${timeStamp}] ${message}`);
+    
+
+  }
+}
--- src/app/tasks/task.service.ts
+++ src/app/tasks/task.service.ts
@@ -1,12 +1,16 @@
-import { Injectable, signal } from "@angular/core";
+import { inject, Injectable, signal } from "@angular/core";
 import { Task, TaskStatus } from "./task.model";
+import { LoggingService } from "../logging.service";
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class TasksService {
-    tasks = signal<Task[]>([])
+   private tasks = signal<Task[]>([])
+    private loggingService = inject(LoggingService)
+
+    allTasks = this.tasks.asReadonly()
 
     addTask(taskData: { title: string; description: string }) {
         const newTask: Task = {
@@ -15,12 +19,15 @@ export class TasksService {
             status: 'OPEN'
         }
         this.tasks.update((oldTasks) => [...oldTasks, newTask])
+        this.loggingService.log(`Task added: ${newTask.title}`)
     }
 
     updateTasksStatus(taskId: string, newStatus: TaskStatus) {
         this.tasks.update((oldTasks) =>
             oldTasks.map((task) =>
                 task.id === taskId ? { ...task, status: newStatus } : task))
+        this.loggingService.log(`Task status updated: ${taskId} to ${newStatus}`)
+    
 
     }
 }
